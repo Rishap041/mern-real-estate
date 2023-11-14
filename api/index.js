5ed50b8ac5b34b1091431c58c68ca8f72b6af2c5
Rishap041 <rishapkumar94@gmail.com>
2023-11-14 23:20:12 +0530

    Create update user API route.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose
   });
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
@@ -28,7 +30,7 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
   return res.status(statusCode).json({
-    success: false, 
+    success: false,
     statusCode,
     message,
   });
